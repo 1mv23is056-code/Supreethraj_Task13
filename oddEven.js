d192e6149a05b65d17f3c64c0bece1e5c599abcc
1mv23is056-code <1mv23is056@sirmvit.edu>
2026-04-13 21:57:41 +0530

    t13

---
+++ oddEven.js
@@ -0,0 +1,21 @@
+let a=2,b=5,c=0,d=7;
+if(a%2==0){
+    console.log("Even");
+} else{
+    console.log("Odd");
+}
+if(b%2==0){
+    console.log("Even");
+} else{
+    console.log("Odd");
+}
+if(c%2==0){
+    console.log("Even");
+} else{
+    console.log("Odd");
+}
+if(d%2==0){
+    console.log("Even");
+} else{
+    console.log("Odd");
+}
